226058e2ce29ec9770ecab182b24cb06b79cef73
Michele Fusaroli <michele.fusaroli@Micheles-MBP.homenet.telecomitalia.it>
2019-04-22 02:44:08 +0200

    Wrangle as a function

--- 1_Wrangle.R
+++ 1_Wrangle.R
@@ -11,85 +11,101 @@ library(tidyverse)
 library(superheat)
 library(RColorBrewer)
 
-# Rda Needed ------------------------------------------------------------------
+# Data Needed ------------------------------------------------------------------
 load("Rda/ICSR_df.Rda")
 load("Rda/AE_list.Rda")
+load("RDA/HCP_df.Rda")
+load("RDA/PZ_df.Rda")
 load("Rda/D_list.Rda")
-ATC <- read_delim("ATC.csv", ";", escape_double = FALSE, 
-                  trim_ws = TRUE)
-ATC <- select(ATC, -"Search term") %>%
-  unique()
-ATC <- ATC[!grepl("&", ATC$Code),]
 D_list <- as.data.frame(D_list)
 D_list <- list(D_list[!grepl("&", D_list$D_list),]) %>%
   unique() %>% 
   unlist() %>%
   trimws()
 
-# Wrangle_df-------------------------------------------------------------------
-# Df with all the data needed already summarized
-Wrangle_df <- data.frame(matrix(ncol = 10, nrow = 0))
-colnames(Wrangle_df) <- c("Drug_Code", "Drug_Name", "AE", "F_EA", "F_nEA", "ROR", "s", "ROR_m", "ROR_M", "IC")
+ATC <- read_delim("ATC.csv", ";", escape_double = FALSE, 
+                  trim_ws = TRUE) %>%
+  select(-"Search term") %>%
+  unique()
+ATC <- ATC[!grepl("&", ATC$Code),]
+
+# Functions -------------------------------------------------------------------
+
 Wrangle <- function(df, D) {
   #
   # Args:
-  #   df  : ICSR_df
-  #   D   : D_list
+  #   df  : Dataframe of observations
+  #   D   : List of drugs
   #
   # Returns:
   #   Wrangle_df : Disproportionality Analysis and summarized data
+  Wrangle_df <- data.frame(matrix(ncol = 10, nrow = 0))
+  colnames(Wrangle_df) <- c("Drug_Code", "Drug_Name", "AE", "F_EA", "F_nEA", "ROR", "s", "ROR_m", "ROR_M", "IC")
   for (i in 1:length(AE_list)) {
     print(i)
+    x <- subset(df, str_detect(Reactions, AE_list[i]))
     for (d in D) {
       print(d)
-      tab <- table(str_detect(df$`Suspect Product Active Ingredients`, d),
-                   str_detect(df$Reactions, AE_list[i]))
-      colnames(tab)[1] <- "nEA" # other reactions
-      colnames(tab)[2] <- "EA"  # reaction e
-      rownames(tab)[1] <- "nF"  # other drugs
-      rownames(tab)[2] <- "F"   # drug d
-      t <- as.data.frame.matrix(tab)
-      F_EA <- t["F","EA"]
-      F_nEA <- t["F","nEA"]
-      ROR_m <- NA
-      ROR_M <- NA
-      ROR <- NA
-      IC <- NA
-      s <- NA
-      if (F_EA>=3) {
-        nF_nEA <- t["nF","nEA"]
-        nF_EA <- t["nF","EA"]
-        ROR <- F_EA * nF_nEA / nF_EA / F_nEA
-        s <- sqrt(1/F_EA + 1/F_nEA + 1/nF_EA + 1/nF_nEA)
-        ROR_m <- exp(log(ROR) - 1.96*s)
-        ROR_M <- exp(log(ROR) + 1.96*s)
-        if (is.infinite(ROR)) {IC <- "[|Inf|]"} else {
-          ROR_m <- round(ROR_m, digits = 1)
-          ROR <- round(ROR, digits = 1)
-          ROR_M <- round(ROR_M, digits = 1)
-          IC <- paste("[", ROR_m,
-                      "|", ROR, "|",
-                      ROR_M, "]",
-                      sep = "")}
+      if (!str_detect(x$`Suspect Product Active Ingredients`, d)) {
+        new_row <- list(d, ATC$Substance[ATC$Code == d], AE_list[i], 0, sum(str_detect(df$`Suspect Product Active Ingredients`, d)), NA, NA, NA, NA, NA)
+      } else {
+        tab <- table(str_detect(df$`Suspect Product Active Ingredients`, d),
+                     str_detect(df$Reactions, AE_list[i]))
+        colnames(tab)[1] <- "nEA" # other reactions
+        colnames(tab)[2] <- "EA"  # reaction e
+        rownames(tab)[1] <- "nF"  # other drugs
+        rownames(tab)[2] <- "F"   # drug d
+        t <- as.data.frame.matrix(tab)
+        F_EA <- t["F","EA"]
+        F_nEA <- t["F","nEA"]
+        ROR_m <- NA
+        ROR_M <- NA
+        ROR <- NA
+        IC <- NA
+        s <- NA
+        if (F_EA>=3) {
+          nF_nEA <- t["nF","nEA"]
+          nF_EA <- t["nF","EA"]
+          ROR <- F_EA * nF_nEA / nF_EA / F_nEA
+          s <- sqrt(1/F_EA + 1/F_nEA + 1/nF_EA + 1/nF_nEA)
+          ROR_m <- exp(log(ROR) - 1.96*s)
+          ROR_M <- exp(log(ROR) + 1.96*s)
+          if (is.infinite(ROR)) {IC <- "[|Inf|]"} else {
+            ROR_m <- round(ROR_m, digits = 1)
+            ROR <- round(ROR, digits = 1)
+            ROR_M <- round(ROR_M, digits = 1)
+            IC <- paste("[", ROR_m,
+                        "|", ROR, "|",
+                        ROR_M, "]",
+                        sep = "")
+            }
+          }
+        new_row <- list(d, ATC$Substance[ATC$Code == d], AE_list[i], F_EA, F_nEA, ROR, s, ROR_m, ROR_M, IC)
       }
-      new_row <- list(d, ATC$Substance[ATC$Code == d], AE_list[i], F_EA, F_nEA, ROR, s, ROR_m, ROR_M, IC)
       Wrangle_df[nrow(Wrangle_df)+1,] <-  new_row
     }
   }
   return(Wrangle_df)
 }
+
+# Wrangled_dfs-------------------------------------------------------------------
+# Df with all the data needed already summarized
 Wrangle_df <- Wrangle(ICSR_df,D_list)
 save(Wrangle_df, file = "Rda/Wrangle_df.Rda")
 
+HCP_Wrangle_df <- Wrangle(HCP_df,D_list) %>%
+  mutate(Reporter_Type = "HCP")
+PZ_Wrangle_df <- Wrangle(PZ_df,D_list) %>%
+  mutate(Reporter_Type = "PZ")
+HCP_PZ_Wrangled_df <- rbind(HCP_Wrangle_df,PZ_Wrangle_df) %>%
+  arrange(Drug_Code, AE)
+save(HCP_PZ_Wrangled_df, file = "Rda/HCP_PZ_Wrangled_df.Rda")
+HCP_PZ_Wrangled <- HCP_PZ_Wrangled_df %>%
+  filter (!is.na(ROR))
+
 # ROR_matrix and IC_matrix ------------------------------------------------------------------
-load("Rda/Wrangle_df.Rda")
-load("Rda/AE_list.Rda")
-load("Rda/D_list.Rda")
-D_list <- as.data.frame(D_list)
-D_list <- list(D_list[!grepl("&", D_list$D_list),]) %>%
-  unique() %>% 
-  unlist() %>%
-  trimws()
+## load("Rda/Wrangle_df.Rda")
+
 Heatmatrix <- matrix(ncol = length(AE_list), nrow = length(D_list))
 rownames(Heatmatrix) <- D_list
 colnames(Heatmatrix) <- AE_list
@@ -182,3 +198,7 @@ superheat(Heatmatrix,
           column.title = "Adverse Event",
           column.title.size = 6)
 dev.off()
+
+# HCP_PZ ----------------------------------------------------------------------
+Wrangle_df <- data.frame(matrix(ncol = 10, nrow = 0))
+colnames(Wrangle_df) <- c("Drug_Code", "Drug_Name", "AE", "F_EA", "F_nEA", "ROR", "s", "ROR_m", "ROR_M", "IC")
