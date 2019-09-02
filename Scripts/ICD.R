34a672d84342c134bf124486761632a30a6b54e5
Michele Fusaroli <michele.fusaroli@Micheles-MBP.homenet.telecomitalia.it>
2019-09-02 13:01:45 +0200

    renamed

--- Scripts/ICD.R
+++ Scripts/ICD.R
@@ -59,8 +59,8 @@ ICD_Char_df <- Describe("hypersexuality")
 ICD_Char_df <- Describe("impulse-control disorder")
 write.csv(ICD_Char_df, file = "Population characteristics/ICD_Char_df.csv")
 
-# 
-
+# Boxplots---------------------------------------------------------------------
+AE_list <- c("gambling", "compulsive shopping", "hypersexuality","impulse-control disorder")
 pdf("Visualization/Boxplots_ICD.pdf")
 for (i in AE_list){
   x <- ROR_df %>%
@@ -78,6 +78,7 @@ for (i in AE_list){
 }
 dev.off()
 
+# Occupancy matrix-------------------------------------------------------------
 ICD_PhD <- read_delim("Databases Used/ICD_PhD.csv", 
                       ";", escape_double = FALSE, trim_ws = TRUE)
 h <- matrix(nrow = 9, ncol = 17)
@@ -121,7 +122,64 @@ superheat(h,
           column.title = "Target",
           column.title.size = 6)
 dev.off()
- 
+
+# Linear Regression Model for ICD ---------------------------------------------
+
+LRM <- function(df, PhD_df){
+  Targets_list <- as.list(unique(PhD_df$Target))
+  Action_list <- as.list(unique(PhD_df$Action))
+  LRM_df <- as.data.frame(matrix(nrow=0, ncol=8))
+  colnames(LRM_df) <- c("AE", "Target","Action", "Intercept", "Slope", "SE", "p_value", "Pearson")
+  pdf("Visualization/ICD_LRM.pdf")
+  for (e in AE_list){
+    x <- subset(df, df$AE == e)
+    x <- subset(x, is.na(x$ROR) == FALSE)
+    x <- subset(x, is.infinite(x$ROR) == FALSE)
+    for (t in Targets_list) {
+      y <- subset(PhD_df, `Target` == t) %>%
+        rename(Drug_Name = Substance)
+      z1 <- left_join(x,y, by ="Drug_Name")
+      z1 <- subset(z1, is.na(z1$Occupancy) == FALSE)
+      for (m in Action_list){
+        z <- subset(z1, z1$Action == m)
+        if (dim(z)[1] >= 4){
+          Intercept <- round(coefficients(summary(lm(z$ROR~z$Occupancy)))[1],2)
+          Slope <- round(coefficients(summary(lm(z$ROR~z$Occupancy)))[2],2)
+          SE <- round(coefficients(summary(lm(z$ROR~z$Occupancy)))[4],2)
+          p_value <- round(coefficients(summary(lm(z$ROR~z$Occupancy)))[8],6)
+          P <- cor.test(z$ROR, z$Occupancy, method="pearson")
+          Pearson <- round(P$estimate,2)
+          LRM_df[nrow(LRM_df)+1,] <- c(e, t, m, Intercept, Slope, SE, p_value, Pearson)
+          if (p_value <= 1){
+            plot1 <- ggplot(data = z, aes(x=z$Occupancy, y=z$ROR, main= paste("ROR ~ ", t))) +
+              geom_smooth(method ="lm") +
+              geom_point(aes(color = z$Drug_Name)) +
+              xlab("Occupancy") +
+              ylab("ROR")
+            title <- ggdraw() + draw_label(paste(e," ~ ", t, "[",m,"]"), fontface = "bold")
+            results <- ggdraw() + draw_label(paste("Intercept: ", Intercept,
+                                                   "     Slope: ", Slope,
+                                                   "     SE: ", SE,
+                                                   "     p-value: ", p_value,
+                                                   "     Pearson: ", Pearson), size = 10)
+            print(plot_grid(title, plot1, results, ncol=1, rel_heights=c(0.1, 1, 0.1)))
+          }
+        }
+      }
+    }
+  }
+  dev.off()
+  LRM_df <- LRM_df %>%
+    arrange(p_value) %>%
+    mutate(Rank = rank(p_value))
+  LRM_df <- LRM_df %>%
+    mutate(BH20 = (Rank/nrow(LRM_df))*0.20) %>%
+    mutate(Sign20 = (p_value <= BH20))
+  ICD_occ_df <- LRM_df
+  write_csv2(ICD2_occ_df, "Results/ICD_occ.csv")
+}
+
+LRM(ROR_df, ICD_PhD)
 
 LRM <- function(df, PhD_df){
   Targets_list <- as.list(unique(PhD_df$Target))
